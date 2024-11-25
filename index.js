3382c6128529499c290973eae13f8b1eff468144
Rahul Barot <rahulbarot411@gmail.com>
2024-11-25 17:35:24 +0530

    Describe your changes here

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n,my name is rahul barot\ni am Devops Engineer at GTCSYS');
+  res.end('Hello, World!\n,my name is rahul barot\ni am Devops Engineer at GTCSYS\nMAHADEV\nMAHADEV');
 });
 
 // Set the server to listen on port 3000
