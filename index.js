6fafe2b65ca3dfe5ad02cf339ebcc9ee1ef97fca
Rahul Barot <rahulbarot411@gmail.com>
2024-11-25 16:40:23 +0530

    Initial commit - Hello World Node.js app

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+// Set the server to listen on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
